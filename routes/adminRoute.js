9e720121bd883c40edad31ebeb1ca9d86dd727cf
typout31 <nicholas.chu98@gmail.com>
2021-11-16 19:04:29 -0800

    Created Admin Page with labels - Currently trying to get session working

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,9 +2,15 @@ const express = require("express");
 const passport = require("../middleware/passport");
 const authController = require("../controller/auth_controller");
 const { forwardAuthenticated, isAdmin } = require("../middleware/checkAuth");
+const { Store } = require("express-session");
 
 const router = express.Router();
 
-router.get("/", isAdmin, (req, res) => res.send("Hello"));
+router.get("/", isAdmin, (req, res) => {
+    res.render("./admin/admin", {
+        user: req.user,
+        session: req.session
+    });
+});
 
 module.exports = router
