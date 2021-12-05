40a78cb67cad32f4db7fa66e7c7516b0045e21aa
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-12-05 14:53:17 -0800

    updated database

--- database.js
+++ database.js
@@ -1,36 +1,40 @@
 let Database = [  
-    {
-    id: 1,
-    name: "Jimmy Smith",
-    role: "user",
-    email: "jimmy123@gmail.com",
-    password: "jimmy123!",
-    reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}, {id: 1, title: "test2", description: "test1", completed: true}]
-  },
   {
-    id: 2,
-    name: "Johnny Doe",
-    role: "user",
-    email: "johnny123@gmail.com",
-    password: "johnny123!",
-    reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
-  },
-  {
-    id: 3,
-    name: "Jonathan Chen",
-    role: "user",
-    email: "jonathan123@gmail.com",
-    password: "jonathan123!",
-    reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
-  },
-  {
-    id: 4,
-    name: "test",
-    type: "Admin",
-    email: "test@test",
-    password: "test",
-    reminders: [{id: 1, title: "test1", description: "test1", completed: false}, {id: 1, title: "test2", description: "test1", completed: true}]
-  }
+  id: 1,
+  name: "Jimmy Smith",
+  role: "user",
+  email: "jimmy123@gmail.com",
+  password: "jimmy123!",
+  reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}, {id: 1, title: "test2", description: "test1", completed: true}],
+  profile_pic: ""
+},
+{
+  id: 2,
+  name: "Johnny Doe",
+  role: "user",
+  email: "johnny123@gmail.com",
+  password: "johnny123!",
+  reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}],
+  profile_pic: ""
+},
+{
+  id: 3,
+  name: "Jonathan Chen",
+  role: "user",
+  email: "jonathan123@gmail.com",
+  password: "jonathan123!",
+  reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}],
+  profile_pic: ""
+},
+{
+  id: 4,
+  name: "test",
+  role: "Admin",
+  email: "test@test",
+  password: "test",
+  reminders: [{id: 1, title: "test1", description: "test1", completed: false}, {id: 1, title: "test2", description: "test1", completed: true}],
+  profile_pic: ""
+}
 ]
 
 module.exports = Database;
